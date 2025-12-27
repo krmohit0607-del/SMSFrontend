e30fc7ccaf3af9fda6db8451142c956fc5f8c54c
kr-mohit06 <mohit.kumar@accelleron-industries.com>
2025-12-27 13:15:16 +0530

    update teachers subject1

--- src/pages/schooladmin/Teachers.jsx
+++ src/pages/schooladmin/Teachers.jsx
@@ -161,7 +161,7 @@ className="mb-4 px-4 py-2 border rounded-lg w-full md:w-1/3"
 <td>
 <span
 className={`px-3 py-1 rounded-full text-xs ${
-t.status === "Active"
+t.isActive
 ? "bg-green-100 text-green-700"
 : "bg-red-100 text-red-700"
 }`}
